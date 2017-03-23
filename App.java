79fac2ec0eb847008b773850e0ee6a5161c1fa2d
Miguel Angel Castillo <castillo.miguel1597@gmail.com>
2017-03-23 08:42:31 -0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,25 @@
+package edu.calstatela.cs.mcastillo.hw2;
+
+public class App {
+	
+	public void runTest() {
+		Deck<Card> deck = new Deck<Card>();
+		
+		// add 52 cards to the deck
+		for (int i = 0; i < 52; i++) {
+			deck.addCard( new Card(i) );
+		}
+
+		deck.shuffle();
+		
+		// print all cards in the deck
+		for (Card c : deck.getCards() ) {
+			System.out.println(c);
+		}
+	}
+	
+	public static void main(String[] args) {
+		new App().runTest();
+	}
+	
+}
---
+++ UniqueHand.java
@@ -0,0 +1,43 @@
+package edu.calstatela.cs.mcastillo.hw2;
+
+
+import java.util.ArrayList;
+
+public class UniqueHand extends Hand {
+
+	ArrayList<Card> getCards() {
+		// Return cards as an ArrayList
+		return null;
+	}
+
+	void removeCard() {
+		// Remove any card from the collection
+	}
+
+	void removeCard(Card card) {
+		// Remove a card that is equivalent to card from the collection
+	}
+	
+	void addCard(Card card) {
+		// Add card to the collection
+	}
+
+	@SuppressWarnings("unused")
+	private MySet<Card> cards;
+
+	public int count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean has(Card card) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int calculateScore() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
